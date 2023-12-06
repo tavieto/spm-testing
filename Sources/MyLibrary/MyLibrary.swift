a5e839dd125a2a18f748047e386b363a6fcf1eb5
Otavio <contato@tavieto.dev>
2023-12-06 18:21:47 -0300

    add

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,3 +1,6 @@
+import SwiftyJSON
+import Foundation
+
 public struct MyLibrary {
     public private(set) var text = "Hello, World!"
 
@@ -7,5 +10,33 @@ public struct MyLibrary {
     
     public func welcome(name: String) -> String {
         return "\(name), bem vindo!"
+    }
+    
+    public func nameToJson(name: String) -> String {
+        let nameStruct: Data = Data()
+        do {
+            let json = try JSON(data: nameStruct)
+        } catch {
+            
+        }
+        return ""
+    }
+}
+
+class NameTest: NSObject, NSCoding {
+    var name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    // MARK: - NSCoding
+
+    required init(coder aDecoder: NSCoder) {
+        self.name = aDecoder.decodeObject(forKey: "name") as? String ?? ""
+    }
+
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: "name")
     }
 }

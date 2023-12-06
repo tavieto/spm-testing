916264caa05d27f4ed0152aa910123011677ff41
Otavio <contato@tavieto.dev>
2023-12-06 16:45:59 -0300

    add

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -2,5 +2,10 @@ public struct MyLibrary {
     public private(set) var text = "Hello, World!"
 
     public init() {
+        print("MyLibrary iniciou")
+    }
+    
+    public func welcome(name: String) -> String {
+        return "\(name), bem vindo!"
     }
 }
